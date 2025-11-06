cfdc5131b6518ba1c76455a08c26a4deda274354
Shatlyk Abdullayev <gj_wp@mail.ru>
2025-11-06 19:03:27 +0700

    feat:job section initial state p1

---
+++ src/components/JobSection/index.tsx
@@ -0,0 +1,59 @@
+'use client'
+import { FC, useState } from 'react';
+import axios from '@/lib/axios'
+
+import { IJobs } from '@/types/job';
+
+//components
+import JobCards from "@/components/JobCards";
+import JobFilters from "@/components/JobFilters";
+import SearchBar from "@/components/SearchBar";
+import { AxiosResponse } from 'axios';
+import { Where } from 'payload';
+import { stringify } from 'qs-esm';
+
+interface Props {
+  initialData: IJobs
+};
+
+function debounce(func: Function, delay:number) {
+  let timeoutId: undefined | ReturnType<typeof setTimeout>;
+
+  return function (...args: any) {
+    clearTimeout(timeoutId)
+
+    timeoutId = setTimeout(() => {
+      // @ts-ignore
+      func.apply(this, args)
+    }, delay)
+  }
+}
+
+const JobSection:FC<Props> = ({initialData}) => {
+  const [data, setData] = useState(initialData)
+
+  if(!initialData) return null
+
+  const fetchJobs = async (query: Where) => {
+    const stringifiedQuery = stringify(
+    {
+      where: query,
+    },
+      { addQueryPrefix: true },
+    )
+
+    const jobs: AxiosResponse<IJobs> = await axios(`/jobs${stringifiedQuery}`);
+    setData(jobs.data)
+  }
+
+  const handleFilterRequest = debounce(fetchJobs, 700)
+
+  return (
+    <div>
+      <JobFilters handleFilterRequest={handleFilterRequest} />
+      <SearchBar />
+      <JobCards jobs={data} />
+    </div>
+  )
+};
+export default JobSection
