308523c6bebdb743f49e2e40acf29c5b949b5f9f
Shatlyk Abdullayev <gj_wp@mail.ru>
2025-11-07 02:42:22 +0700

    add dateConvert function

---
+++ src/components/ui/currency-icon.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+import { TCurrency } from '../../../data/filters';
+import { getCurrencyIcon } from '@/composables/salaryConvert';
+
+interface Props {
+  currency: TCurrency
+  isAvailable: boolean
+};
+
+const CurrencyIcon:FC<Props> = ({currency, isAvailable}) => {
+  if (!currency || !isAvailable) return null;
+  
+  const Icon = getCurrencyIcon(currency);
+
+  if (typeof Icon === "string") {
+    return <span>{Icon}</span>; // Wrap string in a span for rendering
+  }
+  return <Icon className="h-4 w-4" />;
+};
+export default CurrencyIcon
--- src/composables/dateConvert.ts
+++ src/composables/dateConvert.ts
@@ -1,4 +1,4 @@
-const dateConvert = (date: string): string => {
+export const dateConvert = (date: string): string => {
   const dt: Date = new Date(date);
   const now: Date = new Date();
   const diff: number = now.getTime() - dt.getTime();
@@ -8,4 +8,11 @@ const dateConvert = (date: string): string => {
   return "недавно";
 };
 
-export default dateConvert;
+export function isoToDate(isoString: string) {
+  const dt = new Date(isoString);
+  const months = ['', 'января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+  const day = dt.getUTCDate();
+  const month = months[dt.getUTCMonth() + 1];
+  const year = dt.getUTCFullYear();
+  return `${day} ${month} ${year}`;
+}
