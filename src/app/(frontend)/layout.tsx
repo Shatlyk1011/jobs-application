ea570fe275ea182a608f39e66605d1b8fb393f2e
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2025-11-17 16:04:23 +0500

    install toast

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from "sonner";
+
 import "../globals.css";
 
 const InterSans = Inter({
@@ -20,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${InterSans.variable} dark antialiased`}>{children}</body>
+      <Toaster theme="system" position="bottom-left" />
     </html>
   );
 }
--- src/services/useCreateResume.ts
+++ src/services/useCreateResume.ts
@@ -1,5 +1,6 @@
 import { Dispatch, SetStateAction, useState } from "react";
 import axios from '@/lib/axios'
+import { toast } from "sonner"
 
 import { type IResumeForm } from "@/types/resume";
 import { InitialResumeFormState } from "../../data/resume";
@@ -9,6 +10,8 @@ export function useCreateResume(setForm: Dispatch<SetStateAction<IResumeForm>>) 
   const createResume = async (form: IResumeForm) => {
     try { 
       setLoading(true)
+      toast.loading("Отправляем запрос...", { id: "loading-toast-id" })
+
       const req = await axios('/resume', {
         method: 'POST',
         data: JSON.stringify(form)
@@ -16,10 +19,17 @@ export function useCreateResume(setForm: Dispatch<SetStateAction<IResumeForm>>) 
 
       if(req.status === 201) {
         setForm(InitialResumeFormState)
+        toast.success("Успешно. Ваше резюме будет доступно после проверки модератора", {
+          id: "loading-toast-id",
+          duration: 5000,
+        })
       }
 
     } catch (err) {
       console.log('err useResume', err);
+      setForm(InitialResumeFormState)
+      toast.error("Что то пошло не так. Попробуйте еще раз", { id: "loading-toast-id" })
+
     }
     finally {
       setLoading(false)
