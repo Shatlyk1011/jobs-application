20d7036a55d2325200da3293bbf1fe0dbe9631df
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2025-11-18 13:48:51 +0500

    add logo

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,14 +1,18 @@
-import Link from "next/link";
 import { FC } from "react";
+
+import { Logo } from "@/components/ui/Logo";
+import Link from "next/link";
 
 interface Props {}
 
 const Header: FC<Props> = () => {
   return (
     <header className="flex h-16 items-center justify-between gap-5">
-      <Link href="/">Logo</Link>
+      <Link href="/" className="h-16 w-20">
+        <Logo className="w-full h-full text-inherit fill-none" />
+      </Link>
 
-      <nav className="flex flex-1 justify-end">
+      <nav className="flex flex-1 justify-center">
         <ul className="flex items-center gap-6 text-base font-medium tracking-tight">
           <li>
             <Link href={"/jobs"}>Вакансии</Link>
@@ -21,6 +25,8 @@ const Header: FC<Props> = () => {
           </li>
         </ul>
       </nav>
+
+      <p>213</p>
     </header>
   );
 };
