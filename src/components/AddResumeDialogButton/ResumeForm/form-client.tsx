a06d67879499d1c24bae4f06a6ff7231dcdaf512
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2025-11-20 23:01:07 +0500

    clean up

--- src/components/AddResumeDialogButton/ResumeForm/form-client.tsx
+++ src/components/AddResumeDialogButton/ResumeForm/form-client.tsx
@@ -1,5 +1,5 @@
-import { ChangeEvent, Dispatch, FC, SetStateAction } from "react";
 import { cn } from "@/lib/utils";
+import { ChangeEvent, Dispatch, FC, SetStateAction } from "react";
 
 import { CURRENCY, FORMAT, LEVEL, LOCATION, PROFESSION } from "../../../../data/filters";
 
