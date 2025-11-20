3c034a9a5bfc73e029fc0a15d6fc84518661c721
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2025-11-21 00:10:39 +0500

    add create image endpoint

---
+++ src/services/useCreateImage.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:3000/api";
+
+export function useCreateImage() {
+  const createImage = async (file: File):Promise<string> => {
+    
+    try {
+      const formData = new FormData()
+      formData.append('file', file)
+
+      const req = await axios("/media", {
+        baseURL: API_URL,
+        method: "POST",
+        data: formData
+      });
+
+      return req.data.doc.url
+
+    } catch (err) {
+      return ''
+    }
+  };
+
+  return { createImage };
+}
+
+export default useCreateImage;
