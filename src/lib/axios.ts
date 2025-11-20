70ceca86ba1ab13fb8bf616097cff368e510cd42
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2025-11-21 00:58:52 +0500

    add example env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
+import { API_URL } from "@/services/shared/constant";
 
-const API_URL = "http://localhost:3000/api";
 
 const instance = axios.create({
   baseURL: API_URL,
