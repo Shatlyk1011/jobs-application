16e455d7cdc82143681323f943a2243c6310c873
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2025-11-21 13:28:52 +0500

    restore endpoints

--- src/app/(frontend)/(jobs-cv)/jobs/page.tsx
+++ src/app/(frontend)/(jobs-cv)/jobs/page.tsx
@@ -7,28 +7,30 @@ import { IJobs } from "@/types/job";
 import { AxiosResponse } from "axios";
 
 export default async function JobsPage() {
-  // const stringifiedQuery = stringify(
-  // {
-  //   where: {
-  //     isVisible: {
-  //       equals: true,
-  //     },
-  //   },
-  // },
-  // { addQueryPrefix: true },
-  // );
+  const stringifiedQuery = stringify(
+    {
+      where: {
+        isVisible: {
+          equals: true,
+        },
+      },
+    },
+    { addQueryPrefix: true },
+  );
 
-  // const response: AxiosResponse<IJobs> = await axios(`/jobs${stringifiedQuery}`);
+  const response: AxiosResponse<IJobs> = await axios(`/jobs${stringifiedQuery}`);
 
-  // if (!response) {
-  //   return null
-  // }
+  if (!response) {
+    return null
+  }
 
-  // const { data } = response;
+  const { data } = response;
 
   return (
     <main className="h-full w-full">
-      <div className="h-full w-full pt-10">{/* <JobSection initialData={data} /> */}</div>
+      <div className="h-full w-full pt-10">
+        <JobSection initialData={data} />
+      </div>
     </main>
   );
 }
