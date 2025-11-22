1db5832b0fd1b48487dde5175a12706d42ef2145
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2025-11-22 17:56:58 +0500

    add consultation collection

---
+++ src/app/(payload)/collections/Consultation/index.ts
@@ -0,0 +1,74 @@
+import type { CollectionConfig } from "payload";
+
+const Consultation: CollectionConfig = {
+  access: {
+    read: () => true,
+    create: () => true,
+  },
+  admin: {
+    defaultColumns: ["username", "telegram", "resumeLink", "isVisible"],
+  },
+  slug: "consultation",
+  fields: [
+    {
+      name: "username",
+      label: "Имя",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "email",
+      label: "Email",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "telegram",
+      label: "Telegram",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "phoneNumber",
+      label: "Номер телефона",
+      type: "text",
+      required: false,
+    },
+    {
+      name: "requestBody",
+      label: "Запрос",
+      type: "textarea",
+      required: false,
+    },
+    {
+      name: "status",
+      label: "Статус",
+      type: "select",
+      options: [
+        {
+          label: "В ожидании",
+          value: "pending",
+        },
+        {
+          label: "Выполняется",
+          value: "processing",
+        },
+        {
+          label: "Выполнено",
+          value: "done",
+        },
+        {
+          label: "Отказано",
+          value: "rejected",
+        },
+      ],
+      required: false,
+      defaultValue: 'pending',
+      admin: {
+        position: "sidebar",
+      },
+    },
+  ],
+};
+
+export default Consultation;
