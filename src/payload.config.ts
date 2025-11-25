61c0a8d398f4c43e9660763645f6ef634b6f9862
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2025-11-25 14:56:55 +0500

    !change admin panel user access

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,25 +1,32 @@
 import sharp from "sharp";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 
+import path from "path";
+import { fileURLToPath } from "node:url";
+
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { buildConfig } from "payload";
 
 // collections
+import AdminUsers from "./app/(payload)/collections/AdminUsers";
+
 import Jobs from "./app/(payload)/collections/Jobs";
-import Media from "./app/(payload)/collections/Media";
+// import Media from "./app/(payload)/collections/Media";
 import Resume from "./app/(payload)/collections/Resume";
 import Mentors from "./app/(payload)/collections/Mentors";
 import Consultation from "./app/(payload)/collections/Consultation";
+import Base64Images from "./app/(payload)/collections/Base64Images";
 
-import path from "path";
-import { fileURLToPath } from "node:url";
+
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
 export default buildConfig({
   editor: lexicalEditor({}),
+  
   admin: {
+    user: AdminUsers.slug,
     importMap: {
       baseDir: path.resolve(dirname),
       // importMapFile: path.resolve(dirname, "app", "(payload)", "admin", "importMap.js"),
@@ -29,7 +36,7 @@ export default buildConfig({
   // If you'd like to use Rich Text, pass your editor here
 
   // Define and configure your collections in this array
-  collections: [Jobs, Media, Resume, Mentors, Consultation],
+  collections: [Jobs, Resume, AdminUsers, Mentors, Consultation, Base64Images],
 
   // Your Payload secret - should be a complex and secure string, unguessable
   secret: process.env.PAYLOAD_SECRET || "",
