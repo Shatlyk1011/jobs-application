fe6186667733d13155baa85148b244b495ce911a
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2025-11-25 15:20:28 +0500

    add loading component

---
+++ src/components/ui/ScreenLoading.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+interface Props {};
+
+const ScreenLoading:FC<Props> = () => {
+  return (
+    <div className="loader">
+      <div className="justify-content-center primary-loading"></div>
+    </div>
+  )
+};
+export default ScreenLoading
