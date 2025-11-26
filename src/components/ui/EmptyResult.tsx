1845261f5aa2434a694f7a4bcf06b082cac54f5d
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2025-11-26 13:45:40 +0500

    add emptyresult component

---
+++ src/components/ui/EmptyResult.tsx
@@ -0,0 +1,18 @@
+import { cn } from '@/lib/utils';
+import { FC } from 'react';
+
+interface Props {
+  children: string 
+  classes?:string
+};
+
+const EmptyResult:FC<Props> = ({ children, classes }) => {
+  return (
+    <div className={cn("flex w-full flex-col items-center gap-2 py-10 text-center", classes)}>
+      <p className="-tracking-one  font-medium">
+        {children}
+      </p>
+    </div>
+  )
+};
+export default EmptyResult
