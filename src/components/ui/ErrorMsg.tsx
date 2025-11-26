9b62630feb5709ea5a70711f78f4aed09637db54
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2025-11-26 22:56:03 +0500

    add input select inputErr component responsive styles

--- src/components/ui/ErrorMsg.tsx
+++ src/components/ui/ErrorMsg.tsx
@@ -7,6 +7,13 @@ interface Props {
 const ErrorMsg: FC<Props> = ({ errorMsg }) => {
   if (!errorMsg) return null;
 
-  return <span className="text-destructive ml-1 text-[13px] leading-[130%] font-normal">{errorMsg}</span>;
+  console.log('errorMsg', errorMsg.split(' ').slice(1));
+
+  return (
+    <>
+      <span className="text-destructive ml-1 text-[13px] max-sm:text-[12px] max-sm:line-clamp-1 leading-[130%] max-sm:hidden font-normal">{errorMsg}</span>
+      <span className="text-destructive ml-1 text-[13px] max-sm:text-[12px] leading-[130%] font-normal first-letter:uppercase hidden max-sm:inline-block">{errorMsg.split(' ').slice(1).join(' ')}</span>
+    </>
+  );
 };
 export default ErrorMsg;
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -25,7 +25,7 @@ const Input: React.FC<Props> = ({ className, as, type, ...props }) => {
     return (
       // @ts-ignore
       <textarea
-        className="bg-input/30 border-input min-h-[120px] rounded-lg border px-3 py-2 placeholder:font-[inherit] placeholder:text-[14px]"
+        className="bg-input/30 border-input min-h-[120px] text-sm rounded-lg border px-3 py-2 placeholder:font-[inherit] placeholder:text-sm"
         {...props}
         ref={textareaRef}
         onChange={handleInput}
@@ -37,9 +37,9 @@ const Input: React.FC<Props> = ({ className, as, type, ...props }) => {
       type={type}
       data-slot="input"
       className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-sm shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive placeholder:text-[16px]",
+        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive placeholder:text-sm max-sm:placeholder:text-xs",
         className,
       )}
       {...props}
