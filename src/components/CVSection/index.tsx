5bbab96dd6f122085ac2b39e0c51181fabe5ca89
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2025-11-26 22:55:40 +0500

    add fallback text for cv page

--- src/components/CVSection/index.tsx
+++ src/components/CVSection/index.tsx
@@ -1,5 +1,4 @@
-"use client";
-import { FC, useState } from "react";
+import { FC } from "react";
 
 import { IResumes } from "@/types/resume";
 
@@ -12,13 +11,16 @@ interface Props {
 }
 
 const CVSection: FC<Props> = ({ initialData }) => {
-  const [data, setData] = useState(initialData);
 
   return (
     <div>
-      <CVFilters />
+      {initialData.docs.length !== 0 ? (
+        <CVFilters />
+      ) : (
+        <p className="text-center text-lg max-w-[680px] max-lg:text-base max-lg:max-w-[80%] max-sm:max-w-full max-sm:px-4 max-sm:py-4 font-medium -tracking-two px-7 mx-auto leading-[1.3] py-5 bg-popover rounded-2xl">Это займет всего 3 минуты. Заполните Ваше резюме, и мы выставим его на нашей платформе.</p>
+      )}
       {/* <SearchBar /> */}
-      <CVCards resumes={data} />
+      <CVCards resumes={initialData} />
     </div>
   );
 };
